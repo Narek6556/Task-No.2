b2957aa3c8e02225823c19c45dc1870ed4ff16f8
Narek6556 <52848075+Narek6556@users.noreply.github.com>
2019-08-07 17:55:00 +0400

    Add files via upload

---
+++ Task No.2/Task No.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task_No._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = ReadLine();
+            int temp = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] == number[number.Length - 1 - i])
+                {
+                    temp++;
+                }
+                else
+                {
+                    temp = 0;
+                }
+            }
+            if (temp == number.Length)
+            {
+                WriteLine("The number is polindrome...");
+            }
+            else
+            {
+                WriteLine("The number is not polindrome...");
+            }
+
+
+           
+        }
+    }
+}
